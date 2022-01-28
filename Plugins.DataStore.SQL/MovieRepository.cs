24879130514f7974d3d7692e3c7db5e93d2a3fb6
Piotr Kałuziński <piotr.kaluzinski@student.wat.edu.pl>
2022-01-28 21:33:01 +0100

    Movie repository

---
+++ Plugins.DataStore.SQL/MovieRepository.cs
@@ -0,0 +1,60 @@
+﻿using CoreBusiness;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UseCases.DataStorePluginInterfaces;
+
+namespace Plugins.DataStore.SQL
+{
+    public class MovieRepository : IMovieRepository
+    {
+        private readonly CinemaContext db;
+
+        public MovieRepository(CinemaContext db)
+        {
+            this.db = db;
+        }
+
+        public void AddMovie(Movie movie)
+        {
+            db.Movies.Add(movie);
+            db.SaveChanges();
+        }
+
+        public void DeleteMovie(int movieId)
+        {
+            var movie = db.Movies.Find(movieId);
+            if (movie == null) return;
+
+            db.Movies.Remove(movie);
+            db.SaveChanges();
+        }
+
+        public Movie GetMovieById(int movieId)
+        {
+            return db.Movies.Find(movieId);
+        }
+
+        public IEnumerable<Movie> GetMovies()
+        {
+            return db.Movies.ToList();
+        }
+
+        public void UpdateMovie(Movie movie)
+        {
+            var movieToUpdate = db.Movies.Find(movie.MovieId);
+            if (movieToUpdate != null)
+            {
+                movieToUpdate.Title = movie.Title;
+                movieToUpdate.Description = movie.Description;
+                movieToUpdate.Year = movie.Year;
+                movieToUpdate.Length = movie.Length;
+                movieToUpdate.AgeRestriction = movie.AgeRestriction;
+                movieToUpdate.ImageUrl = movie.ImageUrl;
+            }
+            db.SaveChanges();
+        }
+    }
+}
